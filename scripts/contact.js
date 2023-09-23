a1d09ba09a92ec31f1fc03897863b37085a4d533
Ansh-Rajput <anshravikrish425317@gmail.com>
2023-09-23 12:30:11 +0530

    added subscribe functionality in contact page and changed boilerplate of email.

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,11 +3,29 @@ const sendEmail=()=>{
         SecureToken : "ab27377e-ca16-4697-b329-611da68eb601",
         To : 'anshravikrish425317@gmail.com',
         From : document.getElementById("email").value,
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        Subject : "contect from enquiry.",
+        Body : `Name: ${document.getElementById('name').value} 
+                <br> Email: ${document.getElementById('email').value} 
+                <br> Phone no: ${document.getElementById('phoneNo').value} 
+                <br> Message: ${document.getElementById('message').value} 
+            `
     }).then(
       message => {alert(message);
-        console.log(document.getElementById("email").value      )
+        console.log(document.getElementById("email").value)
+    }
+    );
+}
+const onSubscribe=()=>{
+    Email.send({
+        SecureToken : "ab27377e-ca16-4697-b329-611da68eb601",
+        To : document.getElementById("newsletter-email").value,
+        From : 'anshravikrish425317@gmail.com',
+        Subject : "ISOC EUM subscription",
+        Body : `You have subscribed to our newsletter.
+            `
+    }).then(
+      message => {alert(message);
+        console.log(document.getElementById("newsletter-email").value)
     }
     );
 }
